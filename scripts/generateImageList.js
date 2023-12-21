1fbc509db60f92ac1e385f6714554d3d42298386
Cameron Bulock <cameron.bulock@quavo.com>
2023-12-21 12:43:22 -0500

    proper image sizes, lazy load

--- scripts/generateImageList.js
+++ scripts/generateImageList.js
@@ -1,7 +1,8 @@
-import { promises as fs, statSync } from "fs";
-import path from "path";
-import exifParser from "exif-parser";
-import dayjs from "dayjs";
+import { promises as fs, createReadStream } from 'fs';
+import path from 'path';
+import exifParser from 'exif-parser';
+import dayjs from 'dayjs';
+import probe from 'probe-image-size';
 
 async function getEXIFData(filePath) {
   try {
@@ -10,22 +11,30 @@ async function getEXIFData(filePath) {
     const result = parser.parse();
     return result.tags;
   } catch (error) {
-    console.error("Error reading EXIF data:", error);
+    console.error('Error reading EXIF data:', error);
     return null;
   }
 }
 
-function getFileCreationDate(filePath) {
+async function getImageDimensions(filePath, orientation) {
   try {
-    const stats = statSync(filePath);
-    return stats.birthtime;
+    const stream = createReadStream(filePath);
+    const dimensions = await probe(stream);
+    stream.close();
+
+    // Check if orientation requires width and height to be swapped
+    if (orientation >= 5 && orientation <= 8) {
+      return { width: dimensions.height, height: dimensions.width };
+    }
+
+    return { width: dimensions.width, height: dimensions.height };
   } catch (error) {
-    console.error("Error reading file creation date:", error);
-    return null;
+    console.error(`Error getting image dimensions for ${filePath}:`, error.message);
+    return { width: null, height: null };
   }
 }
 
-const imagesDirectory = path.join(process.cwd(), "public", "images");
+const imagesDirectory = path.join(process.cwd(), 'public', 'images');
 
 async function generateImageList() {
   try {
@@ -36,35 +45,24 @@ async function generateImageList() {
       if (/\.(jpg|jpeg|png)$/.test(file)) {
         const filePath = path.join(imagesDirectory, file);
         const exifData = await getEXIFData(filePath);
-        let createdDate = exifData?.DateTimeOriginal;
 
-        if (createdDate) {
-          createdDate = dayjs.unix(createdDate).toISOString();
-        } else {
-          const fileCreationDate = getFileCreationDate(filePath);
-          createdDate = fileCreationDate
-            ? dayjs(fileCreationDate).toISOString()
-            : null;
+        if (exifData?.DateTimeOriginal) {
+          const createdDate = dayjs.unix(exifData.DateTimeOriginal).toISOString();
+          const orientation = exifData.Orientation || 1; // Default orientation is 1
+          const dimensions = await getImageDimensions(filePath, orientation);
+
+          imageFiles.push({ fileName: file, exifData, createdDate, ...dimensions });
         }
-
-        imageFiles.push({ fileName: file, exifData, createdDate });
       }
     }
 
     // Sort images by creation date, newest first
-    imageFiles.sort(
-      (a, b) => dayjs(b.createdDate).unix() - dayjs(a.createdDate).unix()
-    );
+    imageFiles.sort((a, b) => dayjs(b.createdDate).unix() - dayjs(a.createdDate).unix());
 
-    await fs.writeFile(
-      path.join(process.cwd(), "src", "imageList.json"),
-      JSON.stringify(imageFiles, null, 2)
-    );
-    console.log(
-      "Image list with EXIF data, creation dates, and sorted by date generated successfully."
-    );
+    await fs.writeFile(path.join(process.cwd(), 'src', 'imageList.json'), JSON.stringify(imageFiles, null, 2));
+    console.log('Image list with EXIF data, creation dates, dimensions, and sorted by date generated successfully.');
   } catch (error) {
-    console.error("Error generating image list:", error);
+    console.error('Error generating image list:', error);
   }
 }
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
 import imageList from "./imageList.json";
 
+const isDev = true;
+
+const imagePath = ({image, width, height}) => ( isDev ? `/images/${image.fileName}` : `https://jupiter.dog/.netlify/images?url=/images/${image.fileName}&w=${width}&h=${height}`);
+
+const newImageSize = ({width: originalWidth, height: originalHeight}) => {
+  const maxSize = 400;
+  let newWidth, newHeight;
+
+  if (originalWidth > originalHeight) {
+      // Width is the larger dimension
+      newWidth = maxSize;
+      newHeight = (originalHeight / originalWidth) * maxSize;
+  } else {
+      // Height is the larger dimension or they are equal
+      newHeight = maxSize;
+      newWidth = (originalWidth / originalHeight) * maxSize;
+  }
+
+  return { width: Math.round(newWidth), height: Math.round(newHeight) };
+}
+
 function App() {
   return (
     <>
       <h1>Life of Jupiter</h1>
-      {imageList.map((image) => (
-        <img key={image.fileName} src={`https://jupiter.dog/.netlify/images?url=/images/${image.fileName}&w=300`} />
-      ))}
+      {imageList.map((image) => {
+        const {width, height, fileName} = image;
+        const newSizes = newImageSize({width, height})
+        return (<img key={fileName} loading="lazy" src={imagePath({image, width: newSizes.width, height:newSizes.height})} width={newSizes.width} height={newSizes.height} />)
+})}
     </>
   );
 }
