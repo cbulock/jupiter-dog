c99cd2f225d2fc9d10e42f1707763070d7cefdb8
Cameron Bulock <cameron@bulock.com>
2024-01-02 23:10:36 -0500

    remove unused routing

--- src/Admin.jsx
+++
@@ -1,8 +0,0 @@
-import CMS from "netlify-cms";
-
-// Now the registry is available via the CMS object.
-CMS.registerPreviewTemplate("default", Default);
-
-export default () => {
-  return null;
-};
--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-
-// import Admin from "./Admin";
 import Home from "./Home";
 
 import "./App.css";
 
 export default () => (
-  <BrowserRouter>
-    <Routes>
-      <Route index element={<Home />} />
-    </Routes>
-  </BrowserRouter>
+  <>
+    <Home />
+  </>
 );
