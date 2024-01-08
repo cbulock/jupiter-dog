7b4d00ec97b3c49612475a217ea0795646e04bd6
Cameron Bulock <cameron.bulock@quavo.com>
2024-01-08 16:51:58 -0500

    hover effect for head logo

--- src/components/JupiterHead.jsx
+++ src/components/JupiterHead.jsx
@@ -10,9 +10,12 @@ const styles = stylex.create({
   },
   imagewrapper: {
     cursor: "url('paw.svg'), auto",
-    transition: "transform 0.1s ease",
+    transition: "transform 0.1s ease, filter 0.3s ease",
     ":active": {
       transform: "scale(0.95)",
+    },
+    ":hover": {
+      filter: "drop-shadow(0 0 5px rgba(255, 255, 255, 0.75))",
     },
   },
   image: {
