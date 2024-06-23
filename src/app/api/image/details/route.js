20bda8c08c8244f2cffaa8f8af7df15a77e7c7c6
Cameron Bulock <cameron@bulock.com>
2024-06-23 19:01:36 -0400

    add mock data for dev testing

--- src/app/api/image/details/route.js
+++ src/app/api/image/details/route.js
@@ -1,6 +1,11 @@
 import { getStore } from "@netlify/blobs";
+import exampleData from "./example-data.json";
+
+const isDev = process.env.NODE_ENV === "development";
 
 export const GET = async (request, response) => {
+  if (isDev) return Response.json(exampleData);
+
   const siteID = process.env.NETLIFY_SITE_ID;
   const token = process.env.NETLIFY_ACCESS_TOKEN;
   const store = getStore({ name: "jupiter-images-metadata", siteID, token });
@@ -9,11 +14,10 @@ export const GET = async (request, response) => {
   const image = searchParams.get("image");
   const name = `${image}.json`;
 
-  const data = await store.getWithMetadata(name, {
-    type: "json",
-  }) || {};
+  const data =
+    (await store.getWithMetadata(name, {
+      type: "json",
+    })) || {};
 
-  return Response.json({
-    data,
-  });
+  return Response.json(data);
 };
